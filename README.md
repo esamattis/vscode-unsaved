200947d8e1fce17627a0919e4c0427f61e309cad
Esa-Matti Suuronen <esa-matti@suuronen.org>
2018-11-09 23:02:34 +0200

    Also add statusbar item

--- README.md
+++ README.md
@@ -1,13 +1,10 @@
-#YOU HAVE UNSAVED FILES
+# YOU HAVE UNSAVED FILES
 
 I think Visual Studio Code does make it clear enough when you have usaved
-files.
-
-This extension changes the status bar color when you have unsaved files to
-something you can actually notice.
+files. This extension tries to make it more clear!
 
 ### Caveat
 
 This extension edits the `colorCustomizations` option fields `statusBar.background`
 `statusBar.noFolderBackground` and `statusBar.debuggingBackground` so beaware
-that those will reset if you have customized them
+that those will reset if you have customized them.
--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,9 @@
 import * as vscode from "vscode";
 import debounce from "lodash.debounce";
 
+const STATUS_BAR_COLOR = "#af2828";
+const UNSAVED_COLOR = "yellow";
+
 type ColorOptions =
   | {
       "statusBar.background": string;
@@ -9,21 +12,23 @@ type ColorOptions =
     }
   | undefined;
 
-function setColor(background: string) {
+function setHilight() {
   const currentColors: ColorOptions = vscode.workspace
     .getConfiguration("workbench")
     .get("colorCustomizations");
 
   const colors = Object.assign(currentColors || {}, {
-    "statusBar.background": background,
-    "statusBar.noFolderBackground": background,
-    "statusBar.debuggingBackground": background
+    "statusBar.background": STATUS_BAR_COLOR,
+    "statusBar.noFolderBackground": STATUS_BAR_COLOR,
+    "statusBar.debuggingBackground": STATUS_BAR_COLOR
   });
 
   vscode.workspace
     .getConfiguration("workbench")
     .update("colorCustomizations", colors, true);
 }
+
+let STATUS_BAR_ITEM: vscode.StatusBarItem;
 
 function resetColor() {
   const colors: ColorOptions = vscode.workspace
@@ -49,19 +54,38 @@ function updateColor() {
   );
 
   if (hasDirty) {
-    setColor("#af2828");
+    setHilight();
+    STATUS_BAR_ITEM.show();
   } else {
     resetColor();
+    STATUS_BAR_ITEM.hide();
   }
 }
 
 const debouncedUpdate = debounce(updateColor, 500);
 
+function createStatusBarItem() {
+  const item = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Left,
+    99999999 // And you think z-index games are bad! :D
+  );
+
+  item.color = UNSAVED_COLOR;
+  item.text = "UNSAVED";
+  item.tooltip = "You have at least one unsaved file";
+  item.hide();
+
+  return item;
+}
+
 export function activate(context: vscode.ExtensionContext) {
+  STATUS_BAR_ITEM = createStatusBarItem();
+
   const disposable = vscode.workspace.onDidChangeTextDocument(() => {
     debouncedUpdate();
   });
 
+  context.subscriptions.push(STATUS_BAR_ITEM);
   context.subscriptions.push(disposable);
 }
 

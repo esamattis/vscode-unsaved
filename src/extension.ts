4089a6556fe30b3df93d5a0f2819afc76183aa73
Esa-Matti Suuronen <esa-matti@suuronen.org>
2018-11-10 14:52:04 +0200

    Add some indentatin and commas using prettiier

--- src/extension.ts
+++ src/extension.ts
@@ -5,85 +5,85 @@ const STATUS_BAR_COLOR = "#af2828";
 const UNSAVED_COLOR = "yellow";
 
 type ColorOptions =
-  | {
-      "statusBar.background": string;
-      "statusBar.noFolderBackground": string;
-      "statusBar.debuggingBackground": string;
-    }
-  | undefined;
+    | {
+          "statusBar.background": string;
+          "statusBar.noFolderBackground": string;
+          "statusBar.debuggingBackground": string;
+      }
+    | undefined;
 
 function createStatusBarItem() {
-  const item = vscode.window.createStatusBarItem(
-    vscode.StatusBarAlignment.Left,
-    99999999 // And you think z-index games are bad! :D
-  );
+    const item = vscode.window.createStatusBarItem(
+        vscode.StatusBarAlignment.Left,
+        99999999, // And you think z-index games are bad! :D
+    );
 
-  item.color = UNSAVED_COLOR;
-  item.text = "UNSAVED";
-  item.tooltip = "You have at least one unsaved file";
-  item.hide();
+    item.color = UNSAVED_COLOR;
+    item.text = "UNSAVED";
+    item.tooltip = "You have at least one unsaved file";
+    item.hide();
 
-  return item;
+    return item;
 }
 
 class UnsavedTracker {
-  statusBarItem = createStatusBarItem();
-  workbenchConfig = vscode.workspace.getConfiguration("workbench");
+    statusBarItem = createStatusBarItem();
+    workbenchConfig = vscode.workspace.getConfiguration("workbench");
 
-  listener = vscode.workspace.onDidChangeTextDocument(
-    debounce(this.updateHilightStatus.bind(this), 600)
-  );
-
-  updateHilightStatus() {
-    const hasUnsavedFiles = vscode.workspace.textDocuments.some(
-      editor => editor.isDirty
+    listener = vscode.workspace.onDidChangeTextDocument(
+        debounce(this.updateHilightStatus.bind(this), 600),
     );
 
-    if (hasUnsavedFiles) {
-      this.activateStatusBarHilight();
-      this.statusBarItem.show();
-    } else {
-      this.resetStatusBarHilight();
-      this.statusBarItem.hide();
-    }
-  }
+    updateHilightStatus() {
+        const hasUnsavedFiles = vscode.workspace.textDocuments.some(
+            editor => editor.isDirty,
+        );
 
-  activateStatusBarHilight() {
-    const currentColors: ColorOptions = this.workbenchConfig.get(
-      "colorCustomizations"
-    );
-
-    const colors = Object.assign(currentColors || {}, {
-      "statusBar.background": STATUS_BAR_COLOR,
-      "statusBar.noFolderBackground": STATUS_BAR_COLOR,
-      "statusBar.debuggingBackground": STATUS_BAR_COLOR
-    });
-
-    this.workbenchConfig.update("colorCustomizations", colors, true);
-  }
-
-  resetStatusBarHilight() {
-    const colors: ColorOptions = this.workbenchConfig.get(
-      "colorCustomizations"
-    );
-
-    if (!colors) {
-      return;
+        if (hasUnsavedFiles) {
+            this.activateStatusBarHilight();
+            this.statusBarItem.show();
+        } else {
+            this.resetStatusBarHilight();
+            this.statusBarItem.hide();
+        }
     }
 
-    delete colors["statusBar.background"];
-    delete colors["statusBar.noFolderBackground"];
-    delete colors["statusBar.debuggingBackground"];
+    activateStatusBarHilight() {
+        const currentColors: ColorOptions = this.workbenchConfig.get(
+            "colorCustomizations",
+        );
 
-    this.workbenchConfig.update("colorCustomizations", colors, true);
-  }
+        const colors = Object.assign(currentColors || {}, {
+            "statusBar.background": STATUS_BAR_COLOR,
+            "statusBar.noFolderBackground": STATUS_BAR_COLOR,
+            "statusBar.debuggingBackground": STATUS_BAR_COLOR,
+        });
 
-  dispose() {
-    this.statusBarItem.dispose();
-    this.listener.dispose();
-  }
+        this.workbenchConfig.update("colorCustomizations", colors, true);
+    }
+
+    resetStatusBarHilight() {
+        const colors: ColorOptions = this.workbenchConfig.get(
+            "colorCustomizations",
+        );
+
+        if (!colors) {
+            return;
+        }
+
+        delete colors["statusBar.background"];
+        delete colors["statusBar.noFolderBackground"];
+        delete colors["statusBar.debuggingBackground"];
+
+        this.workbenchConfig.update("colorCustomizations", colors, true);
+    }
+
+    dispose() {
+        this.statusBarItem.dispose();
+        this.listener.dispose();
+    }
 }
 
 export function activate(context: vscode.ExtensionContext) {
-  context.subscriptions.push(new UnsavedTracker());
+    context.subscriptions.push(new UnsavedTracker());
 }
